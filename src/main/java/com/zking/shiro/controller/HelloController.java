b4ced8ea24e0f5844da72259b29c36834505569c
lisiting2021 <2300652798@qq.com>
2021-01-05 23:40:19 +0800

    第二个版本

--- src/main/java/com/zking/shiro/controller/HelloController.java
+++ src/main/java/com/zking/shiro/controller/HelloController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //@Controller
 public class HelloController {
 
+
+
 //    @RequestMapping("/")
     public String hello(){
         return "login";
